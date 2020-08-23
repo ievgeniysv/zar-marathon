53af90fbe1aaa74aae6dbac428e2131554d34e51
Evgeniy <ievgeniysv@gmail.com>
2020-08-23 15:01:32 +0500

    homework3

--- main.js
+++ main.js
@@ -1,65 +1,55 @@
-//Задание 1
+const btnCharacterElem = document.getElementById('character-kick')
+const btnEnemyElem = document.getElementById('enemy-kick')
 
-const firstRow = 'мамА мыла раму';
-const secondRow = 'собака друг человека';
+const character = {
+  name: 'Pikachu',
+  defaultHp: 100,
+  damageHp: 100,
+  elHp: document.getElementById('health-character'),
+  elProgressbar: document.getElementById('progressbar-character')
+}
 
-// решение через ругулярные выражения
-function getRowRegExp(firstRow, secondRow) {
-  if (((firstRow.match(/а/gi) || []).length) > ((secondRow.match(/а/gi) || []).length)) {
-    return firstRow;
-  } else if (((firstRow.match(/а/gi) || []).length) < ((secondRow.match(/а/gi) || []).length)) {
-    return secondRow;
+const enemy = {
+  name: 'Charmander',
+  defaultHp: 100,
+  damageHp: 100,
+  elHp: document.getElementById('health-enemy'),
+  elProgressbar: document.getElementById('progressbar-enemy')
+}
+
+btnCharacterElem.addEventListener('click', () => {
+  changeHp(random(20),enemy)
+})
+
+btnEnemyElem.addEventListener('click', () => {
+  changeHp(random(20),character)
+})
+
+function init() {
+  console.log('start game')
+  renderHp(character)
+  renderHp(enemy)
+}
+
+function renderHp(person){
+  person.elHp.innerText = person.damageHp + '/' + person.defaultHp
+  person.elProgressbar.style.width = person.damageHp + '%'
+}
+
+function changeHp(count, person) {
+  if(person.damageHp < count){
+    person.damageHp = 0
+    renderHp(person)
+    btnCharacterElem.disabled = true
+    alert(person.name + ' проиграл!')
   } else {
-    return 'Колличество букв а в строках одинаково';
+    person.damageHp -= count;
   }
-}
-console.log(getRowRegExp(firstRow, secondRow))
-
-//Решение через цикл
-
-function getRow(firstRow, secondRow) {
-
-  let firstQuantity = 0;
-  for(let i = 0; i < firstRow.length; i++){
-    if(firstRow[i] === 'а' || firstRow[i] === 'А' ) firstQuantity++;
-  }
-
-  let secondQuantity = 0;
-  for(let i = 0; i < secondRow.length; i++){
-    if(secondRow[i] === 'а' || secondRow[i] === 'А') secondQuantity++;
-  }
-
-  if(firstQuantity > secondQuantity) {
-    return firstRow;
-  } else if (firstQuantity < secondQuantity){
-    return secondRow;
-  }else {
-    return 'Колличество букв а в строках одинаково';
-  }
+  renderHp(person)
 }
 
-console.log(getRow(firstRow, secondRow));
-
-//Задание 2
-//
-function formattedPhone(phone) {
-  let formattedStr = '';
-  for(let i = 0; i < phone.length; i++){
-    if(i === 2){
-      formattedStr += ' ('
-    }
-    if(i === 5){
-      formattedStr += ') '
-    }
-    if(i === 8){
-      formattedStr += '-';
-    }
-    if(i === 10){
-      formattedStr += '-';
-    }
-    formattedStr += phone[i];
-  }
-  return formattedStr;
+function random (num) {
+  return Math.ceil(Math.random()*num)
 }
 
-console.log(formattedPhone('+71234567890'));
+init()
